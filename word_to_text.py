7b90a8420f4d15c23227b53c608171250fd792c5
Aaron Mathias <Southwick3574@users.noreply.github.com>
2020-03-08 09:08:41 -0400

    reuploaded word_to_text file

---
+++ word_to_text.py
@@ -0,0 +1,21 @@
+import docx
+# pip install python-docx
+
+file_name = input("Please give me a word document file name: ")
+
+
+def wordtotext(name):
+    doc = docx.Document(name)
+
+    text = ''
+    for paragraph in doc.paragraphs:
+        text += paragraph.text
+        text += '\n'
+    return text
+
+
+print(wordtotext(file_name))
+text_file = open("Output.txt", "w")
+text_file.write(wordtotext(file_name))
+text_file.write('\n')
+text_file.close()
